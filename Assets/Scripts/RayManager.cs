c1fe94955b22f61060d4b451aecfd0f3cb7094c2
LeeJoon <leejun9334@gmail.com>
2023-05-31 15:35:00 +0900

    [update] TestCode

--- Assets/Scripts/RayManager.cs
+++ Assets/Scripts/RayManager.cs
@@ -23,15 +23,45 @@ public class RayManager : MonoBehaviour
     {
         if(Input.touchCount > 0) {
             Touch touch = Input.GetTouch(0);
-            Vector2 screenCenterPos = main.ViewportToScreenPoint(touch.position);
-            if (arRaycastManager.Raycast(screenCenterPos, hits, TrackableType.PlaneWithinPolygon))
+            //Vector2 screenCenterPos = main.ViewportToScreenPoint(touch.position);
+            //if (arRaycastManager.Raycast(screenCenterPos, hits, TrackableType.PlaneWithinPolygon))
+            //{
+            //    if (hits.Count > 0)
+            //    {
+            //        // compare object
+            //        Debug.Log($"{hits[0].trackable}");
+            //        temptext.text = $"{hits[0].trackable}\n{hits[0].pose.position}\n{transform.position}\nName:{hits[0].trackable.name}";
+            //    }
+            //}
+
+            Vector3 touchPos;
+            Ray ray;
+            RaycastHit hit;
+            switch (touch.phase)
             {
-                if (hits.Count > 0)
-                {
-                    // compare object
-                    Debug.Log($"{hits[0].trackable}");
-                    temptext.text = $"{hits[0].trackable}\n{hits[0].pose.position}\n{transform.position}\nName:{hits[0].trackable.name}";
-                }
+                case TouchPhase.Began:
+                    Vector3 touchPosToVector3 = new Vector3(touch.position.x, touch.position.y, 100);
+                    touchPos = Camera.main.ScreenToWorldPoint(touchPosToVector3);
+                    ray = Camera.main.ScreenPointToRay(touchPosToVector3);
+
+                    if (Physics.Raycast(ray,out hit, 100))
+                    {
+                        if (hit.collider.CompareTag("Enemy"))
+                        {
+                            temptext.text = $"Name : {hit.transform.name}";
+                        }
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    break;
+                case TouchPhase.Stationary:
+                    break;
+                case TouchPhase.Ended:
+                    break;
+                case TouchPhase.Canceled:
+                    break;
+                default:
+                    break;
             }
         }
     }
